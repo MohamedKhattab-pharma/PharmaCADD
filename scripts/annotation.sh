c90a10a50af7a0218fe5ccb90a6d9748772b8667
Mohamed S Khattab <mohameds.h.khattab@gmail.com>
2026-04-25 21:35:04 +0300

    Update VEP input files to Final_SNPS and Final_INDELS

--- scripts/annotation.sh
+++ scripts/annotation.sh
@@ -1,9 +1,8 @@
 
 ### Technical Annotation
 
-vep -i SNPS_filtered.vcf -o Annotated_snp.vcf --vcf --species homo_sapiens 
+vep -i Final_SNPS.vcf -o Annotated_snp.vcf --vcf --species homo_sapiens 
     --assembly GRCh38 --cache--offline --fasta GRCh38.dna.toplevel.fa.gz --everything
 
-vep -i INDELs_filtered.vcf -o Annotated_indels.vcf --vcf --species homo_sapiens 
+vep -i Final_INDELS.vcf -o Annotated_indels.vcf --vcf --species homo_sapiens 
     --assembly GRCh38 --cache--offline --fasta GRCh38.dna.toplevel.fa.gz --everything
-    
